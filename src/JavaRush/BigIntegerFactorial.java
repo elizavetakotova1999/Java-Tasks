14d8024ddad5e4db3adc5170cdfe3bfbf4de3cd0
Elizaveta Kotova <kotakoray@gmail.com>
2020-09-06 23:32:26 +0300

    Added task "BigIntegerFactorial"

--- src/JavaRush/BigIntegerFactorial.java
+++ src/JavaRush/BigIntegerFactorial.java
@@ -1,19 +1,16 @@
 package JavaRush;
-
 import java.math.BigInteger;
+
+/*Реализуйте метод, вычисляющий факториал заданного натурального числа.
+Факториал N вычисляется как 1 * 2 * ...* N.
+Поскольку это очень быстро растущая функция, то даже для небольших N
+вместимости типов int и long очень скоро не хватит. Поэтому будем использовать BigInteger.*/
 
 public class BigIntegerFactorial {
     public static void main(String[] args) {
         System.out.println(factorial(456));
     }
-    /**
-     * Calculates factorial of given <code>value</code>.
-     *
-     * @param value positive number
-     * @return factorial of <code>value</code>
-     */
     public static BigInteger factorial(int value) {
         return value <= 0 ? BigInteger.ONE : BigInteger.valueOf(value).multiply(factorial(value - 1));
-
     }
 }
