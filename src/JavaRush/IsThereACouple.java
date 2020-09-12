365539a92e725ef435a6f84adb22bd7f068400a9
Elizaveta Kotova <kotakoray@gmail.com>
2020-09-12 22:42:29 +0300

    Added task "IsThereACouple"

---
+++ src/JavaRush/IsThereACouple.java
@@ -0,0 +1,55 @@
+package JavaRush;
+
+import java.util.Scanner;
+
+/*
+Существует ли пара?
+Ввести с клавиатуры три целых числа. Определить, имеется ли среди них хотя бы одна пара равных между собой чисел.
+Если такая пара существует, вывести на экран числа через пробел.
+Если все три числа равны между собой, то вывести все три.
+
+Примеры:
+а) при вводе чисел
+1
+2
+2
+получим вывод
+2 2
+
+б) при вводе чисел
+2
+2
+2
+получим вывод
+2 2 2
+
+Требования:
+1. Программа должна считывать числа c клавиатуры.
+2. Программа должна содержать System.out.println() или System.out.print()
+3. Если два числа равны между собой, необходимо вывести числа на экран.
+4. Если все три числа равны между собой, необходимо вывести все три.
+5. Если нет равных чисел, ничего не выводить.
+*/
+
+import java.util.Scanner;
+
+public class IsThereACouple {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a == b && b == c) {
+            System.out.println(a + " " + b + " " + c);
+        } else if (a == b) {
+            System.out.println(a + " " + b);
+        } else if (b == c) {
+            System.out.println(b + " " + c);
+        } else if (a == c) {
+            System.out.println(a + " " + c);
+        } else {
+
+        }
+    }
+}
