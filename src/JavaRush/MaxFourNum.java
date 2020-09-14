5419cbc2cb2c154798a24cb2d2773864fd66da2b
Elizaveta Kotova <kotakoray@gmail.com>
2020-09-14 21:17:59 +0300

    Added task "MaxFourNum"

---
+++ src/JavaRush/MaxFourNum.java
@@ -0,0 +1,32 @@
+package JavaRush;
+import java.util.Scanner;
+/*
+Максимум четырех чисел
+Ввести с клавиатуры четыре числа, и вывести максимальное из них.
+Если числа равны между собой, необходимо вывести любое.
+
+
+Требования:
+1. Программа должна считывать числа c клавиатуры.
+2. Программа должна выводить число на экран.
+3. Программа должна выводить на экран максимальное из четырёх чисел.
+4. Если максимальных чисел несколько, необходимо вывести любое из них.
+*/
+
+
+
+public class MaxFourNum {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        if (a != b && b != c && c != d) {
+            int max = Math.max(Math.max(a, b), Math.max(c, d));
+            System.out.println(max);
+        } else {
+            System.out.println(a);
+        }
+    }
+}
