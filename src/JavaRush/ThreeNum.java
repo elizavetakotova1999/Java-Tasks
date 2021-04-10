fbdacb7660cc59e4a282a3c1b4f79777d4c88895
Elizaveta Kotova <kotakoray@gmail.com>
2021-04-10 23:06:15 +0300

    Added task "FaceControl"

---
+++ src/JavaRush/ThreeNum.java
@@ -0,0 +1,21 @@
+package JavaRush;
+
+import java.util.Scanner;
+
+public class ThreeNum{
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a == b) {
+            if (c != a) {
+                System.out.println(3);
+            }
+        } else if (b == c) {
+            System.out.println(1);
+        } else if (c == a) {
+            System.out.println(2);
+        }
+    }
+}
