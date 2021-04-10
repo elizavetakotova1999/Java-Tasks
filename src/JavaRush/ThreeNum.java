1e6d946a431cfba88ff21b7d047868da2d599876
Elizaveta Kotova <kotakoray@gmail.com>
2021-04-10 23:12:17 +0300

    delete throws Exception

--- src/JavaRush/ThreeNum.java
+++ src/JavaRush/ThreeNum.java
@@ -3,7 +3,7 @@ package JavaRush;
 import java.util.Scanner;
 
 public class ThreeNum{
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
