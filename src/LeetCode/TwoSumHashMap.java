6432c0831eaf0d93e338eeabdf967fa24c9ebca3
Elizaveta Kotova <kotakoray@gmail.com>
2021-04-27 17:10:39 +0300

    Added task "TwoSumHashMap" to LeetCode

--- src/LeetCode/TwoSumHashMap.java
+++ src/LeetCode/TwoSumHashMap.java
@@ -22,6 +22,9 @@ public class TwoSumHashMap {
     public int[] twoNum(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],i);
+        }
+        for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
                     return new int[]{i, j};
