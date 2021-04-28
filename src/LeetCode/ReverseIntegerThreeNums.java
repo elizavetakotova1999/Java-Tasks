9ee1451ad2418cdc6f01015e4da51fa68852cf1f
Elizaveta Kotova <kotakoray@gmail.com>
2021-04-28 23:09:08 +0300

    Added task "ReverseIntegerThreeNums" to LeetCode.

--- src/LeetCode/ReverseIntegerThreeNums.java
+++ src/LeetCode/ReverseIntegerThreeNums.java
@@ -1,7 +1,5 @@
 package LeetCode;
-/*Given a signed 32-bit integer x, return x with its digits reversed.
- If reversing x causes the value to go outside the signed 32-bit integer
- range [-231, 231 - 1], then return 0.*/
+
 public class ReverseIntegerThreeNums {
     public static void main(String[] args) {
         ReverseIntegerThreeNums reverseInteger = new ReverseIntegerThreeNums();
