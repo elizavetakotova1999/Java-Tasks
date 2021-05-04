b6ca41f7bb2915d737716490299f82a20b316af2
Elizaveta Kotova <kotakoray@gmail.com>
2021-05-04 21:57:44 +0300

    Added task "ReverseLinkedList" to LeetCode.

---
+++ src/LeetCode/ReverseLinkedList.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+    }
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+
+            return head;
+        }
+    }
+}
