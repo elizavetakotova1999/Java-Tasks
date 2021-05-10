3849b824b2e5c9e1fd115710fed0c3a0a5584b60
Elizaveta Kotova <kotakoray@gmail.com>
2021-05-11 00:15:17 +0300

    Added task "RemoveDuplicatesFromSortedArray" to LeetCode.

---
+++ src/LeetCode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,14 @@
+package LeetCode;
+//Учитывая отсортированный массив nums, удалите дубликаты на месте,
+// чтобы каждый элемент появлялся только один раз и возвращал новую длину.
+
+//Не выделяйте дополнительное пространство для другого массива, вы должны сделать это,
+// изменив входной массив на месте с помощью дополнительной памяти O(1).
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+
+    }
+    public int removeDuplicates(int[] nums) {
+
+    return 0;}
+}
