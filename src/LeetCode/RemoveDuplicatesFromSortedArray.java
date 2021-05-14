451f849bcca4d69ea5861e29eb0b3e4975e7f377
Elizaveta Kotova <kotakoray@gmail.com>
2021-05-14 04:40:59 +0300

    Added task "RemoveDuplicatesFromSortedArray" to LeetCode.

--- src/LeetCode/RemoveDuplicatesFromSortedArray.java
+++ src/LeetCode/RemoveDuplicatesFromSortedArray.java
@@ -9,6 +9,14 @@ public class RemoveDuplicatesFromSortedArray {
 
     }
     public int removeDuplicates(int[] nums) {
-
-    return 0;}
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+  }
 }
