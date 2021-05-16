693fbf2a403f1542f297ecd6bac6f20c350a364a
Elizaveta Kotova <kotakoray@gmail.com>
2021-05-17 02:16:28 +0300

    Added "ObjMethods" to MyTasks.

--- src/MyTasks/ObjMethods.java
+++ src/MyTasks/ObjMethods.java
@@ -2,8 +2,18 @@ package MyTasks;
 
 public class ObjMethods {
     public static void main(String[] args) {
-
-
+//http://developer.alexanderklimov.ru/android/java/object.php
+//protected Object clone() - создает новый объект, не отличающийся от клонируемого.
+//public boolean equals(Object obj) - определяет, равен ли один объект другому.
+//protected void finalize() - вызывается перед удалением неиспользуемого объекта.
+//public final Class<?> getClass() - получает класс объекта во время выполнения.
+//public int hashCode() - возвращает хэш-код, связанный с вызывающим объектом.
+//public final void notify() - возобновляет исполнение потока, ожидающего вызывающего объекта.
+//public final void notifyAll() - возобновляет исполнение всех потоков, ожидающих вызывающего объекта.
+//public String toString() - возвращает символьную строку, описывающую объект.
+//public final void wait() - ожидает другого потока исполнения.
+//public final void wait(long timeout) - ожидает другого потока исполнения.
+//public final void wait(long timeout, int nanos) - ожидает другого потока исполнения.
 
     }
 }
