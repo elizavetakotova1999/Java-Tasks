bf11ac403f9d019e88347114fbae856352026072
Elizaveta Kotova <kotakoray@gmail.com>
2021-05-21 03:58:34 +0300

    Added "DeepCloneStudent" to MyTasks.

---
+++ src/MyTasks/CloneObj/DeepCloneStudent.java
@@ -0,0 +1,32 @@
+package MyTasks.CloneObj;
+
+import MyTasks.ParameterizedType.Mark;
+
+import java.util.ArrayList;
+
+public class DeepCloneStudent implements Cloneable {
+    private int id = 71;
+    private ArrayList<Mark> lm = new ArrayList<Mark>();
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public ArrayList<Mark> getMark() {
+        return lm;
+    }
+    public void setMark(ArrayList<Mark> lm) {
+        this.lm = lm;
+    }
+    public Object clone() {
+        try {
+            DeepCloneStudent copy =(DeepCloneStudent)super.clone();
+            copy.lm = (ArrayList<Mark>)lm.clone();
+            return copy;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(
+                    "отсутствует Cloneable!");
+        }
+    }
+}
