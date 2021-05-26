68283044f1c819a9377e3eba272101693a32d8c6
Elizaveta Kotova <kotakoray@gmail.com>
2021-05-26 08:12:32 +0300

    Added "TEST" to MyTasks.

--- src/MyTasks/TEST.java
+++ src/MyTasks/TEST.java
@@ -1,62 +1,73 @@
 package MyTasks;
 
-import java.util.Arrays;
-
 public class TEST {
+    static int[] arrInstance = new int[100];
+
     public static void main(String[] args) {
 
 
+
+        String s = new String("ssssss");
+        StringBuffer sb = new StringBuffer("bbbbbb");
+        s.concat("-aaa");//неизменяемая строка  - но если написать String s2 = s.concat("-aaa"); - работает,
+        // тогда мы присваиваем новую ссылку
+        sb.append("-aaa");
+        System.out.println(s);
+        System.out.println(sb);
+
+        /*class A {
+            private int counter = 0;
+            public static int getInstanceCount() {
+                return counter;
+            }
+            public A() {
+                counter++;
+            }
+        }
+        public class B {
+            public static void main(String ar[]) {
+                A a1 = new A();
+                A a2 = new A();
+                A a3 = new A();
+                System.out.println(A.getInstanceCount()); //1
+            }
+        } //Нельзя использовать в статическом методе нестатические поля класса. */
+
+        byte[] arrLocal = new byte[100];
+        for (int i = 0; i < 100; i++)
+            if (arrInstance[i] != arrLocal[i]) {
+                throw new IllegalStateException("error");//компиляция пройдет успешно и программа выполнится без ошибок
+            }
+        //При сравнении произойдет приведение типов byte -> int, а так как условие 0 != 0 всегда будет ложным,
+        // то в тело if поток выполнения программы не зайдет и исключение выброшено не будет.
+
+        Boolean b = new Boolean("Hello");//false
+        System.out.println(b);
+        //Конструктор создаёт объект со значением true, только если переданная строка совпадает,
+        // без учёта регистра, с "true".
+        // Во всех остальных случаях создаётся объект со значением false.
+
+        final long String = 0;
+        final long Char = 0;
+        final long string = 0;
+        //final long byte/int/char = 0;
+        final long Byte = 0;
+        //ключевые слова языка Java, такие как названия примитивных типов byte и char,
+        // не могут быть использованы в качестве имён переменных.
+        //String, Byte, Char, не относятся к ключевым словам(т.к имена классов можно) и их разрешается использовать в качестве имён переменных.
+
+        int x = 0;
+        System.out.print(x++ == ++x);
+        //Пояснение: - сначала будет взято значение x и запомнено как левая часть выражения (т.е. 0)
+        //- потом будет произведён пост-инкремент (т.е. x примет значение 1)
+        //- потом выполнится пре-инкремент (т.е. x примет значение 2)
+        //- и только теперь будет взято значение для правого операнда операции сравнения (т.е. 2), таким образом получаем сравнение "0 == 2".
     }
+
+    /* public static void main(String\u005B\u005D args) { // 2 //все ок
+        char a = '\u005B'; // 3
+        char c = '\u005D'; // 4
+        System.out.println(a * c); // 5
+    }*/
+
 }
-/*
-//class
-//__________________________________________________________________________________
-//ключевые слова
-//abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract abstract
-//assert assert assert assert assert assert assert assert assert assert assert assert assert assert assert assert assert assert
-boolean boolean boolean boolean boolean boolean boolean boolean boolean boolean boolean boolean boolean boolean boolean
-break break break break break break break break break break break break break break break break break break break break break break
-byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte byte break
-//case case case case case case case case case case case case case case case case case case case case case case case
-//catch catch catch catch catch catch catch catch catch catch catch catch catch catch catch case case catch catch catch catch catch
-char char char char char char char char char char char char
-class class class class class class class class class class class class class class class class class class class class class class
-//continue continue continue continue continue continue continue continue continue continue continue continue continue continue continue continue continue
-//default default default default default default default default default default default default default
-do do do do do do
-double double double double double double double double
-else else else else else else else else else else else else else else else else
-enum enum enum enum enum enum enum enum enum enum enum
-extends  extends extends extends extends extends extends extends extends extends extends extends extends
-final final final final final final final final final final final final final final final final final final final final
-finally finally finally finally finally finally finally finally finally finally finally finally finally finally
-float float float float float float float float float float float float float float float float float float
-for for for for for for for for for for for for for for for for for for for for for for for for for for for for for for for for
-if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if if
-implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements implements
-import import import import import import import import import import import import java.util.Arrays; import java.util.Arrays; import java.util.Arrays;
-//instanceof instanceof instanceof instanceof instanceof instanceof instanceof instanceof instanceof instanceof instanceof instanceof
-int int int int int int int int
-interface interface interface interface interface interface interface interface interface interface interface interface interface interface interface interface
-long long long long long long long long long long long long long long long long long long long long int interface instanceof
-//native native native native native native native native native native native native native native native native native
-new new new new new new new new new new new new new new new new new new new new new new new new new new new new new new new new
-package package package package package package package package package package package package package
-private private private private private private private private private private private private private private private private private
-protected protected protected protected protected protected protected protected protected protected
-public public public public public public public public public public public public public public public public public public public
-return return return return return return return return return return return return return return return return return return return return return
-short short short short short short
-static static static static static static static static static static static static static static static static static static static static static static static static static static static
-//strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp strictfp
-//super super super super super super super super super super super super super super super super super super super super super super
-switch switch switch switch switch switch switch switch switch switch switch switch switch switch switch switch switch switch switch switch
-//synchronized synchronized synchronized synchronized synchronized synchronized synchronized synchronized synchronized synchronized
-this this this this this this this this this this this this this this this this
-throw throw throw throw throw throw throw throw throw throw throw throw throw throw throw
-throws throws throws throws throws throws throws throws throws throws throws throws throws throws throws throws throws throw
-//transient transient transient transient transient transient transient transient transient transient transient transient transient
-//try try try try try try try try try try try try try try try try try try try try try try try try try try try try try try try try try
-void void void void void void void void void void void void void void void void void void void void void void void void void void void void void vodi
-//volatile volatile volatile volatile volatile volatile volatile volatile volatile volatile volatile volatile volatile volatile volatile volatile
-while while while while while while while while while while*/
